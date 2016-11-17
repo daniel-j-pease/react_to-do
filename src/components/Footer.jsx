056cbd285fe83dcc8ee11fc9ddfbd149ac011f12
Daniel Pease <daniel.j.pease@gmail.com>
2016-11-17 17:04:45 -0500

    Something

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React, {Components} from 'react';
+import React, {Component} from 'react';
 
 export default class Footer extends Component {
   render() {
@@ -6,7 +6,7 @@ export default class Footer extends Component {
       <footer className="footer">
         <div className="footer-copyright">
           <div className="container">
-            <p>Made with <span className="glyphicon glyphicon-heart"></span> by <a href="https://generalassemb.ly/instructors/jason-seminara/6831">Jason Seminara</a> in 2016 for <span class="gaLogo"></span> in New York City.</p>
+            <p>Made with <span className="glyphicon glyphicon-heart"></span> by <a href="https://generalassemb.ly/instructors/jason-seminara/6831">Jason Seminara</a> in 2016 for <span className="gaLogo"></span> in New York City.</p>
           </div>
         </div>
       </footer>
